04b928ece2d533efdfb6a5e8a7abcbc63b00bd3a
Jeremy <jjm5843@rit.edu>
2018-07-18 18:04:47 -0400

    query methods now return result sets but parsing functions are still included just not in use

--- src/Models/Vendor.java
+++ src/Models/Vendor.java
@@ -30,7 +30,8 @@ public class Vendor {
     //Queries//
 
     //select all for a vendor based on id
-    public static Vendor getSingleVendorQuery(DatabaseController dbController, StatementTemplate stmtUtil, int id){
+    //parsing method still included but this just returns the ResultSet right now
+    public static ResultSet getSingleVendorQuery(DatabaseController dbController, StatementTemplate stmtUtil, int id){
 
         Statement stmt = stmtUtil.newNullStatement();
         ResultSet rs = null;
@@ -51,13 +52,14 @@ public class Vendor {
             System.out.println("Error Executing Select Query for Vendor");
         }
 
-        Vendor vendor = parseResultSet(rs);
+        //Vendor vendor = parseResultSet(rs); // parsing the query
 
-        return vendor;
+        return rs;
     }
 
     //selects names of all vendors
-    public static ArrayList<String> getListOfVendors(DatabaseController dbController, StatementTemplate stmtUtil, int id){
+    //returns ResultSet but code for parsing is still included
+    public static ResultSet getListOfVendors(DatabaseController dbController, StatementTemplate stmtUtil, int id){
 
         Statement stmt = stmtUtil.newNullStatement();
         ResultSet rs = null;
@@ -78,8 +80,8 @@ public class Vendor {
             System.out.println("Error Executing Select Query for Vendor");
         }
 
-        ArrayList<String> vendorNames = parseResultSetList(rs);
-        return vendorNames;
+        //ArrayList<String> vendorNames = parseResultSetList(rs); //calls the parsing method
+        return rs;
 
     }
 
@@ -87,6 +89,7 @@ public class Vendor {
 
 
     //Utils//
+    //not used because we are assuming that parsing is going on on the GUI end
 
     //gets a vendor from a query
     private static Vendor parseResultSet(ResultSet rs){
