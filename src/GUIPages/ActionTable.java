a741979e405ab12275818067f56bf01e4f53ca01
Channing <cxh1845@rit.edu>
2018-07-18 21:10:19 -0400

    Actiontable is functional on the table part, and tested. Action stuff incoming

--- src/GUIPages/ActionTable.java
+++ src/GUIPages/ActionTable.java
@@ -1,10 +1,7 @@
 package GUIPages;
 
 import Controllers.GuiController;
-import com.googlecode.lanterna.gui2.Direction;
-import com.googlecode.lanterna.gui2.Label;
-import com.googlecode.lanterna.gui2.LinearLayout;
-import com.googlecode.lanterna.gui2.Panel;
+import com.googlecode.lanterna.gui2.*;
 import com.googlecode.lanterna.gui2.table.Table;
 
 import java.sql.ResultSet;
@@ -26,6 +23,7 @@ public class ActionTable implements iPage
         panel.addComponent(new Label(PageName));
         int colcount = 0;
         ArrayList<String> headers = null;
+        ArrayList<String> colNames = null;
         ArrayList<Integer> colSizes = null;
         ArrayList<Integer> colTypes = null;
         try //Get and unpack the metadata
@@ -33,15 +31,17 @@ public class ActionTable implements iPage
             ResultSetMetaData rsmd = rs.getMetaData();
             colcount = rsmd.getColumnCount();
             headers = new ArrayList<>(colcount);
+            colNames = new ArrayList<>(colcount);
             colSizes = new ArrayList<>(colcount);
             colTypes = new ArrayList<>(colcount);
+
             for (int i = 1; i <= colcount; i++)
             {
-                headers.add(rsmd.getColumnName(i));
+                headers.add(rsmd.getColumnLabel(i));
+                colNames.add(rsmd.getColumnName(i));
                 colSizes.add(rsmd.getColumnDisplaySize(i));
                 colTypes.add(rsmd.getColumnType(i));
             }
-
 
         }
         catch (SQLException e)
@@ -52,8 +52,36 @@ public class ActionTable implements iPage
 
 
         Table<String> data = new Table<String>(headers.toArray(new String[colcount]));
+        try
+        {//So we might want to have two try catch blocks here. I didn't for time, but right now, if there's an error
+            // ANYWHERE in the table, it can't display ANY of it. This is something that we might want to change if
+            // we run into later problems
+            rs.beforeFirst();
+            while (rs.next())
+            {
+                String[] row = new String[colcount];
+                for (int i = 0; i < colcount; i++)
+                {
+                    row[i] = (rs.getString(colNames.get(i)));
+                }
+                data.getTableModel().addRow(row);
+            }
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
 
+        }
         panel.addComponent(data);
+        panel.addComponent(new Button("Back", new Runnable()
+        {
+            @Override
+            public void run()
+            {
+
+                guiController.closePage();
+            }
+        }));
 
     }
 
