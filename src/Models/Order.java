5fe726cac97a3e48bed5014652c0db1ff4755408
Jeremy <jjm5843@rit.edu>
2018-07-18 21:34:16 -0400

    added TODO and includes getOrdersByStoreIdQuery

--- src/Models/Order.java
+++ src/Models/Order.java
@@ -25,6 +25,10 @@ public class Order {
         this.orderNum = orderNum;
     }
 
+    //Queries//
+    //TODO Find out if we want to get all of the products ordered with the intital query or do we want just the
+    //TODO ordernum, storeid, and customerId to be selectable and when selected itll show all of the products purchased
+
     //get all columns for a customer
     public static ArrayList<Order> getOrdersByCustIdQuery(DatabaseController dbController, StatementTemplate stmtUtil, int id){
 
@@ -50,6 +54,34 @@ public class Order {
 
         return parseResultSet(rs);
     }
+
+    //get all orders for a store
+    public static ResultSet getOrdersByStoreIdQuery(DatabaseController dbController, StatementTemplate stmtUtil, int id){
+        Statement stmt = stmtUtil.newNullStatement();
+        ResultSet rs = null;
+
+        String selectCustomer = "SELECT * FROM Orders WHERE storeId =" + id;
+
+        //create query statement
+        try {
+            stmt = stmtUtil.connStatement(stmt);
+        }catch(Exception e){
+            System.out.println("Error Creating Fetch Statement for Order");
+        }
+
+        //execute and get results of query
+        try {
+            rs = dbController.ExecuteSelectQuery(stmt, selectCustomer);
+        }catch(Exception e){
+            System.out.println("Error Executing Query for Order");
+            e.printStackTrace();
+        }
+
+        //return parseResultSet(rs); //used for when returning an arraylist
+        return rs;
+    }
+
+    //Utils//
 
     //create cust object based off query
     private static ArrayList<Order> parseResultSet(ResultSet rs) {
@@ -77,6 +109,8 @@ public class Order {
 
         return orderHistory;
     }
+
+
 
     public String viewOrderDetails(){
         String orderDetails = "Order Number:" + this.orderNum +'\n';
