1a96b85bc2ed92e87a52dee78123a1ee42f05b0f
Jeremy <jjm5843@rit.edu>
2018-07-25 17:49:50 -0400

    Added Top20Prod for a specific store and overall and added working tests in MetricsTest

---
+++ Tests/Tests/MetricsTest.java
@@ -0,0 +1,76 @@
+package Tests;
+
+import Controllers.DatabaseController;
+import Models.Metrics;
+import Utilities.StatementTemplate;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MetricsTest {
+
+    Connection conn;
+    DatabaseController dbController;
+    StatementTemplate stmtUtil;
+
+    private void initialize() throws Exception{
+
+        conn = DriverManager.getConnection("jdbc:h2:./Tests", "sa", "");
+
+        dbController = new DatabaseController(conn);
+        stmtUtil = new StatementTemplate(conn);
+
+
+        dbController.InitializeNewDatabaseInstance();
+    }
+
+
+    @Test
+    void topTwentyProdStoreTest() throws Exception{
+        initialize();
+
+        int store_Id = 1; //testing store with id 1
+
+        ResultSet rs = Metrics.TopTwentyProdStore(store_Id);
+        assertNotNull(rs);
+        System.out.println(rs);
+        System.out.println("\n                          || Top Twenty Products For Store " + store_Id + " ||\n");
+        while(rs.next()){
+            String string = String.format("%1$15s %2$-40s  %3$10s", rs.getString(1), "| "+rs.getString(2), "|      "+rs.getString(3));
+            //System.out.println(rs.getString("Name") + " | " + rs.getString("Total_Sold"));
+            System.out.println(string);
+        }
+        /*
+        System.out.println(rs.getMetaData().getColumnName(1));
+        System.out.println(rs.getMetaData().getColumnName(2));
+        System.out.println(rs.getMetaData().getColumnDisplaySize(1));
+        System.out.println(rs.getMetaData().getColumnDisplaySize(2));
+        System.out.println(rs.next());
+        System.out.println(rs);
+        System.out.println(rs.next());
+        System.out.println(rs);
+        System.out.println(rs.getString("NAME"));
+        System.out.println(rs.getString("TOTAL_SOLD"));
+        */
+
+    }
+
+    @Test
+    void TopTwentyProdOverallTest() throws Exception{
+        initialize();
+
+        ResultSet rs = Metrics.TopTwentyProdOverall();
+        assertNotNull(rs);
+        System.out.println(rs);
+        System.out.println("\n                          || Top Twenty Products ||\n");
+        while(rs.next()){
+            String string = String.format("%1$15s %2$-40s  %3$10s", rs.getString(1), "| "+rs.getString(2), "|      "+rs.getString(3));
+            System.out.println(string);
+        }
+
+    }
+}
--- src/Models/Metrics.java
+++ src/Models/Metrics.java
@@ -1,5 +1,9 @@
 package Models;
 
+import Controllers.DatabaseController;
+
+import java.sql.ResultSet;
+
 /**
  * Created by Channing Helmling-Cornell on 7/19/2018.
  * <p>
@@ -8,4 +12,35 @@ package Models;
  */
 public class Metrics
 {
+
+    public static ResultSet TopTwentyProdStore(int store_Id)
+    {
+
+        return DatabaseController.SelectQuery("WITH productWithOrderNum as ( " +
+                                                    "SELECT UPC, name, orderNum, quantity " +
+                                                    "FROM Product join prodQuantities on " +
+                                                    "Product.UPC = prodQuantities.productUPC) " +
+                                                "SELECT UPC, Name, sum( quantity ) as Total_Sold " +
+                                                "FROM productWithOrderNum join Orders on " +
+                                                "productWithOrderNum.orderNum = Orders.orderNum " +
+                                                "WHERE storeId = "+ store_Id + " " +
+                                                "GROUP BY UPC, Name " +
+                                                "ORDER BY Total_Sold DESC " +
+                                                "LIMIT 20", false);
+    }
+
+    public static ResultSet TopTwentyProdOverall()
+    {
+
+        return DatabaseController.SelectQuery("WITH productWithOrderNum as ( " +
+                                                    "SELECT Product.UPC, Product.name, prodQuantities.orderNum, prodQuantities.quantity " +
+                                                    "FROM Product join prodQuantities on " +
+                                                    "Product.UPC = prodQuantities.productUPC) " +
+                                                "SELECT UPC, Name, sum( quantity ) as Total_Sold " +
+                                                "FROM productWithOrderNum join Orders on " +
+                                                "productWithOrderNum.orderNum = Orders.orderNum " +
+                                                "GROUP BY UPC, Name " +
+                                                "ORDER BY Total_Sold DESC " +
+                                                "LIMIT 20", false);
+    }
 }
